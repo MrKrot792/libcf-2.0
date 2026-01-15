e08e181b842138b807f6526a3fa84d3d067fb712
Vladislav Korotianets <vkorotanec@gmail.com>
2026-01-15 18:08:31 +0100

    First release, i guess it works, but no features yet

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,91 @@
-const root = @import("root.zig");
+const std = @import("std");
+const lcf = @import("root.zig");
+const rl = @import("raylib");
+
+// Conway's game of life:
+// const state = bool;
+//
+// pub fn tick(previous: [9]?state) state {
+//     var neighbors: u32 = 0;
+//
+//     for (previous) |value| { 
+//         if( value orelse false ) neighbors += 1; 
+//     }
+//
+//     if (previous[4].?) neighbors -= 1;
+//
+//     if (previous[4].?) {
+//         return (neighbors == 2 or neighbors == 3);
+//     } else {
+//         return neighbors == 3;
+//     }
+// }
+//
+// pub fn drawAs(cell: state) rl.Color {
+//     if (cell) return rl.Color.red
+//     else return rl.Color.black;
+// }
+
+const state = u8;
+
+pub fn tick(previous: [9]?state) state {
+    var sum: u32 = 0;
+
+    for (previous) |value| sum += value orelse previous[4].?;
+
+    const avg: u8 = @intCast(sum / 9);
+    var new_val: i16 = @as(i16, @intCast(avg)) + 5 - (@rem(@as(i16, @intCast(avg)), 10));
+    if (new_val < 0) new_val = 0;
+    if (new_val > 255) new_val = 255;
+
+    var random = std.Random.DefaultPrng.init(@bitCast(std.time.microTimestamp()));
+    if (random.random().float(f32) > 0.995) new_val = 255;
+    if (random.random().float(f32) > 0.995) new_val = 0;
+
+    return @intCast(new_val);
+}
+
+pub fn drawAs(cell: state) rl.Color {
+    return .init(
+        cell, 255 - cell, cell / 2, 255,
+    );
+}
 
 pub fn main() !void {
-    root.helloWorld();
+
+    const allocator = std.heap.smp_allocator;
+    rl.initWindow(800, 800, "libCF test");
+
+    // Cellular automaton initialization
+    var grid: lcf.grid(.{100, 100}, state) = try .init(allocator, tick, drawAs);
+    defer grid.deinit(allocator);
+
+    // Filling the grid with random values
+    var random = std.Random.DefaultPrng.init(@bitCast(std.time.microTimestamp()));
+    for (grid.data) |*value| { value.* = random.random().int(u8); }
+
+    var frame: u64 = 0;
+    rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        rl.clearBackground(.ray_white);
+
+        std.debug.print("Ticking... x{d}\n", .{frame});
+        
+        try grid.tick(allocator); 
+
+        grid.draw(.{ 
+            .position = .{0, 0}, 
+            .dimensions = .{800, 800}
+        });
+
+        rl.drawFPS(0, 0);
+
+        if (rl.isKeyPressed(.r)) { for (grid.data) |*value| { value.* = random.random().int(u8); } }
+
+        rl.endDrawing();
+
+        frame += 1;
+    }
 }
--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,101 @@
 const std = @import("std");
+const rl = @import("raylib");
 
-pub fn helloWorld() void {
-    std.debug.print("Hello, world!\n", .{});
+const vec2 = [2]i32;
+
+const square: type = struct {
+    position: vec2,
+    dimensions: vec2,
+};
+
+pub fn grid(comptime grid_size: vec2, comptime cell_type: type) type {
+    return struct {
+        /// Pointer to the grid of cells
+        data: []cell_type,
+
+        /// For internal use only.
+        tickFn: *const fn ([9]?cell_type) cell_type,
+        /// For internal use only.
+        drawFn: *const fn (cell_type) rl.Color,
+        
+        /// You always gotta call `deinit()`.
+        /// BTW, fourth element of the first argument in `tickFn` is always available (.? is always safe to use)
+        pub fn init(
+            allocator: std.mem.Allocator,
+            tickFn: *const fn ([9]?cell_type) cell_type,
+            drawFn: *const fn (cell_type) rl.Color
+        ) !@This() {
+            return .{ 
+                .data = try allocator.alloc(cell_type, grid_size[0] * grid_size[1]),
+                .tickFn = tickFn, .drawFn = drawFn
+            };
+        }
+
+        /// Call this after using the object
+        pub fn deinit(this: *@This(), allocator: std.mem.Allocator) void {
+            allocator.free(this.data);
+        }
+
+        /// You gotta call ts in a loop btw
+        /// BTW, `where.dimensions` is absolute 
+        pub fn draw(this: @This(), where: square) void {
+            for (0..grid_size[1]) |y_u| {
+                for (0..grid_size[0]) |x_u| {
+                    const y: i32 = @intCast(y_u);
+                    const x: i32 = @intCast(x_u);
+                    
+                    const square_size_x = @divFloor(where.dimensions[0], grid_size[0]);
+                    const square_size_y = @divFloor(where.dimensions[1], grid_size[1]);
+                    
+                    // Current cell
+                    rl.drawRectangle(
+                        where.position[0] + square_size_x * x,  // Position X
+                        where.position[1] + square_size_y * y,  // Position Y
+                        square_size_x, // Width
+                        square_size_y, // Height
+                        this.drawFn(this.data[y_u * @as(usize, grid_size[0]) + x_u])   // Color (we get it from a user-defined function)
+                    );
+                }
+            }
+        }
+
+        /// Hopefully this works 🙏
+        pub fn tick(this: *@This(), allocator: std.mem.Allocator) !void {
+            var data: []cell_type = try allocator.alloc(cell_type, grid_size[0] * grid_size[1]);
+            defer allocator.free(data);
+
+            for (0..grid_size[1]) |y| {
+                for (0..grid_size[0]) |x| {
+                    var current_neihbors: [9]?cell_type = @splat(null);
+                    var available_neihbors: [9]bool = @splat(false);
+
+                    available_neihbors[0] = !(y == 0 or x == 0);                  // up+left
+                    available_neihbors[1] = !(y == 0);                            // up
+                    available_neihbors[2] = !(y == 0 or x == (grid_size[0] - 1)); // up+right
+
+                    available_neihbors[3] = !(x == 0);                  // left
+                    available_neihbors[4] = true;                       // center
+                    available_neihbors[5] = !(x == (grid_size[0] - 1)); // right
+
+                    available_neihbors[6] = !(y == (grid_size[1] - 1) or x == 0);                  // down+left
+                    available_neihbors[7] = !(y == (grid_size[1] - 1));                            // down
+                    available_neihbors[8] = !(y == (grid_size[1] - 1) or x == (grid_size[0] - 1)); // down+right
+
+                    if (available_neihbors[0]) current_neihbors[0] = this.data[(y - 1) * grid_size[0] + (x - 1)];
+                    if (available_neihbors[1]) current_neihbors[1] = this.data[(y - 1) * grid_size[0] + (x)];
+                    if (available_neihbors[2]) current_neihbors[2] = this.data[(y - 1) * grid_size[0] + (x + 1)];
+                    if (available_neihbors[3]) current_neihbors[3] = this.data[(y) * grid_size[0] + (x - 1)];
+                    if (available_neihbors[4]) current_neihbors[4] = this.data[(y) * grid_size[0] + (x)];
+                    if (available_neihbors[5]) current_neihbors[5] = this.data[(y) * grid_size[0] + (x + 1)];
+                    if (available_neihbors[6]) current_neihbors[6] = this.data[(y + 1) * grid_size[0] + (x - 1)];
+                    if (available_neihbors[7]) current_neihbors[7] = this.data[(y + 1) * grid_size[0] + (x)];
+                    if (available_neihbors[8]) current_neihbors[8] = this.data[(y + 1) * grid_size[0] + (x + 1)];
+
+                    data[y * grid_size[0] + x] = this.tickFn(current_neihbors);
+                }
+            }
+
+            @memmove(this.data, data);
+        }
+    };
 }
